8b1702355dc31fba65d3f13e7bfdddfa8e367243
Kamil Zakiev <v-kazaki@microsoft.com>
2015-08-12 11:59:32 -0700

    Update phantomjs.ps1

--- phantomjs.ps1
+++ phantomjs.ps1
@@ -1,9 +1,32 @@
+$zipUrl = "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.0.0-windows.zip"
+$zipFile = "phantomjs-2.0.0-windows.zip"
+$zipDir = "$PSScriptRoot\extracted"
+$phantomJsBinDir = "$zipDir\phantomjs-2.0.0-windows\bin"
+
+echo "phantomJsBinDir $phantomJsBinDir"
+
 $webclient = New-Object System.Net.WebClient
-$url = "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.0.0-windows.zip"
-$file = "phantomjs-2.0.0-windows.zip"
-$webclient.DownloadFile($url,$file)
-dir
+$webclient.DownloadFile($zipUrl,$zipFile)
+
+If (Test-Path $zipDir){
+  Remove-Item $zipDir -Force -Recurse
+}
+
 Add-Type -assembly "system.io.compression.filesystem"
 [io.compression.zipfile]::ExtractToDirectory("phantomjs-2.0.0-windows.zip", "extracted")
-copy "extracted/phantomjs-2.0.0-windows/bin/phantomjs.exe" phantomjs.exe
+
 Get-ChildItem
+
+echo "$PSScriptRoot" 
+echo $PSScriptRoot
+
+echo "First Print Path"
+echo ($env:Path).Replace(';',"`n")
+
+echo "Set Path"
+$env:Path += ";" + $phantomJsBinDir
+
+echo "Second Print Path"
+echo ($env:Path).Replace(';',"`n")
+
+dir $phantomJsBinDir
